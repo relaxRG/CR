cdf557b350f1b63917ff903704b08a0a68a7454e
relaxRG <3.01608568e+08+relaxRG@users.noreply.github.com>
2026-08-03 16:21:35 +0000

    Build 127: bump iOS buildNumber to 127 for TestFlight

--- app.config.ts
+++ app.config.ts
@@ -50,7 +50,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "120",
+    buildNumber: "127",
     "infoPlist": {
       "ITSAppUsesNonExemptEncryption": false,
       "UIRequiresFullScreen": false
